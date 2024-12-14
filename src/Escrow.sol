0e06e4367bb4d7fedf9fab773b5233dd35acd697
Taylor Webb <tbwebb22@gmail.com>
2024-12-14 10:47:03 -0500

    init commit

---
+++ src/Escrow.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract Escrow {
+    uint256 public number;
+
+    function sponsor(uint256 amount) public {
+
+    }
+}
